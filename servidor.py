e285911973edb3d09af17bec3b9ad6c451ba8b8a
AlanSaid1 <99698053+AlanSaid1@users.noreply.github.com>
2022-10-25 08:29:15 -0500

    Cambios

---
+++ servidor.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify, render_template
+
+#Generar el servidor en Flask(backend)
+servidorWeb = Flask(__name__)
+
+#Anotacion
+@servidorWeb.route("/test", methods=['GET'])
+def formulario():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    servidorWeb.run(debug = False, host = '0.0.0.0', port = '8080')
